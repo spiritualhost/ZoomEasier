75f7c97e277156459893eb397db564c026789b57
Ryan Piazza <burntfurniture@email.com>
2025-07-21 15:42:51 -0400

    Back to pyinstaller?

--- install.py
+++ install.py
@@ -1,6 +1,5 @@
 import subprocess, sys, logging
 from func import whichPlatform
-from macSetup import py2app
 
 logger = logging.getLogger(__name__)
 
@@ -8,10 +7,6 @@ def installApp():
     try:
         subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
         print("Requirements installed successfully!")
-
-        whereami = whichPlatform()
-        if whereami == "Mac":
-            py2app()
 
     except Exception as e:
         print("Failed to install requirements.")
